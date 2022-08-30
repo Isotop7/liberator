fda3dcf264a9ca42a35a3572d38642c1e0dab87c
Isotop7 <hendrik@hr94.de>
2022-08-30 19:45:17 +0200

    add bulma and templating

--- main.go
+++ main.go
@@ -13,13 +13,6 @@ import (
 
 var DB *gorm.DB
 
-// Shelve
-type Shelve struct {
-	gorm.Model
-	Location string `json:"location"`
-	Content  []Book `json:"content"`
-}
-
 // Book
 type Book struct {
 	gorm.Model
@@ -42,7 +35,6 @@ func connectDatabase() {
 	}
 
 	db.AutoMigrate(&Book{})
-	db.AutoMigrate(&Shelve{})
 
 	DB = db
 }
@@ -211,88 +203,10 @@ func deleteBookEndpoint(ctx *gin.Context) {
 	}
 }
 
-// List all shelves
-func listShelvesEndpoint(ctx *gin.Context) {
-	var shelves = []Shelve{}
-	err := DB.Find(&shelves)
-	if err != nil {
-		ctx.JSON(http.StatusOK, shelves)
-	} else {
-		ctx.JSON(http.StatusNotFound, gin.H{
-			"message": "error getting list of shelves",
-		})
-	}
-}
-
-// List single shelve
-func listShelveEndpoint(ctx *gin.Context) {
-	// Get parameter from request
-	idParam := ctx.Param("id")
-
-	// Parse id to int
-	id, err := strconv.Atoi(idParam)
-	// If no int
-	if err != nil {
-		ctx.JSON(http.StatusNotFound, gin.H{
-			"message": "shelve not found",
-		})
-		return
-	}
-	// If negative value
-	if id <= 0 {
-		ctx.JSON(http.StatusNotFound, gin.H{
-			"message": "shelve not found",
-		})
-		return
-	}
-
-	// Create and find shelve
-	var shelve = Shelve{}
-	dbErr := DB.Where("id = ?", id).First(&shelve)
-
-	// If shelve was found
-	if dbErr != nil && shelve.ID > 0 {
-		ctx.JSON(http.StatusOK, shelve)
-		return
-	} else {
-		ctx.JSON(http.StatusNotFound, gin.H{
-			"message": "shelve not found",
-		})
-		return
-	}
-}
-
-// Create single shelve
-// TODO: Handle books in content body
-func createShelveEndpoint(ctx *gin.Context) {
-	var newElement Shelve
-
-	// Bind body data to element
-	err := ctx.BindJSON(&newElement)
-	if err != nil {
-		switch err.(type) {
-		case *json.UnmarshalTypeError:
-			ctx.JSON(http.StatusBadRequest, gin.H{
-				"message": "Invalid request",
-			})
-		}
-		return
-	}
-
-	// Check if element with id is already present in db and fail
-	var duplicateShelve Shelve
-	_ = DB.Where("id = ?", newElement.ID).First(&duplicateShelve)
-	if duplicateShelve.ID == newElement.ID {
-		ctx.JSON(http.StatusConflict, gin.H{
-			"message": "duplicate element with id",
-			"data":    newElement,
-		})
-		return
-	} else {
-		// Create element in db
-		DB.Create(&newElement)
-		ctx.JSON(http.StatusCreated, newElement)
-	}
+func showDashboard(ctx *gin.Context) {
+	ctx.HTML(http.StatusOK, "dashboard.tmpl", gin.H{
+		"title": "liberator",
+	})
 }
 
 // Main function
@@ -305,10 +219,11 @@ func main() {
 	// Setup handlers
 	router := gin.Default()
 
-	// Shelves
-	router.GET("/shelves", listShelvesEndpoint)
-	router.GET("/shelves/:id", listShelveEndpoint)
-	router.POST("/shelves", createShelveEndpoint)
+	// Index handler
+	router.GET("/", func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, "/dashboard")
+	})
+	router.GET("/dashboard", showDashboard)
 
 	// Books
 	router.GET("/books", listBooksEndpoint)
@@ -316,6 +231,12 @@ func main() {
 	router.GET("/books/:id", listBookEndpoint)
 	router.PUT("/books/:id", updateBookEndpoint)
 	router.DELETE("/books/:id", deleteBookEndpoint)
+
+	// Serve static files
+	router.Static("/static", "./static")
+
+	// Load templates
+	router.LoadHTMLGlob("templates/*")
 
 	// Serve API
 	log.Fatal(router.Run(":5000"))
