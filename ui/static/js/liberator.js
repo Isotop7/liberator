3046c248525ed3175ea5911b3aba8d573cbbb855
Hendrik Röder <hendrik@hr94.de>
2022-09-14 21:19:17 +0200

    add method to update slider (#19)

--- ui/static/js/liberator.js
+++ ui/static/js/liberator.js
@@ -1,2 +1,15 @@
 // Attach to slider
 bulmaSlider.attach()
+
+slider = document.getElementById("sliderWithValue");
+slider.addEventListener("input", () => {
+    sliderOutput = document.getElementById("sliderOutput");
+    val = parseInt(slider.value)
+    if (val < 4) {
+        sliderOutput.className = "has-text-black has-background-danger"
+    } else if (val < 8) {
+        sliderOutput.className = "has-text-black has-background-warning"
+    } else {
+        sliderOutput.className = "has-text-black has-background-success"
+    }
+});
